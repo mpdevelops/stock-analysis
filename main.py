ff27738c8e1cf2f01756e229a25ba34c5ea9f8e5
Mike Paiva <mikepaiva@resnet300-passy-ip-10-36-33.153.resnet.yorku.ca>
2021-03-04 15:08:47 -0500

    parse html data into list

--- main.py
+++ main.py
@@ -14,7 +14,23 @@ for ticker in tickers:
     html = BeautifulSoup(response, 'html')
     news_table = html.find(id='news-table')
     news_tables[ticker] = news_table
-
     break
 
-print(news_tables)
+parsed_data = []
+
+for ticker, news_table in news_tables.items():
+
+    for row in news_table.findAll('tr'):
+
+        title = row.a.text
+        date_data = row.td.text.split(' ')
+
+        if len(date_data) == 1:
+            time = date_data[0]
+        else:
+            date = date_data[0]
+            time = date_data[1]
+
+        parsed_data.append([ticker, date, time, title])
+
+print(parsed_data)
