b6d19be23504507d0d3fc029a8c5663b185025e4
Mike Paiva <mikepaiva@resnet300-passy-ip-10-36-33.153.resnet.yorku.ca>
2021-03-04 16:03:53 -0500

    added visualization of sentiment analysis with MatPlotLib

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
+import matplotlib.pyplot as plt
 
 finviz_url = 'https://finviz.com/quote.ashx?t='
 tickers = ['AMZN', 'MSFT', 'GOOG']
@@ -16,7 +17,6 @@ for ticker in tickers:
     html = BeautifulSoup(response, 'html')
     news_table = html.find(id='news-table')
     news_tables[ticker] = news_table
-    break
 
 parsed_data = []
 
@@ -41,3 +41,12 @@ vader = SentimentIntensityAnalyzer()
 
 f = lambda title: vader.polarity_scores(title)['compound']
 df['compound'] = df['title'].apply(f)
+df['date'] = pd.to_datetime(df.date).dt.date
+
+plt.figure(figsize=(10, 8))
+
+mean_df = df.groupby(['ticker', 'date']).mean()
+mean_df = mean_df.unstack()
+mean_df = mean_df.xs('compound', axis="columns").transpose()
+mean_df.plot(kind='bar')
+plt.show()
